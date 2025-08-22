c8d9585c04260f30f8d20a226e8d3a579303b6dc
rajandran <rajandran@marketcalls.in>
2025-08-22 06:32:28 +0530

    1min and Daily Historical Data Support and Quotes

--- Plugin.cpp
+++ Plugin.cpp
@@ -1,10 +1,12 @@
-// Plugin.cpp - Fixed version with working status display and funds endpoint
+// Plugin.cpp - Complete implementation with Quotes and Historical data
 #include "stdafx.h"
 #include "resource.h"  // Include resource definitions
 #include "OpenAlgoGlobals.h"
 #include "Plugin.h"
 #include "Plugin_Legacy.h"
 #include "OpenAlgoConfigDlg.h"
+#include <math.h>
+#include <time.h>
 
 // Plugin identification
 #define PLUGIN_NAME "OpenAlgo Data Plugin"
@@ -54,12 +56,33 @@ static struct RecentInfo* g_aInfos = NULL;
 static int RecentInfoSize = 0;
 static BOOL g_bPluginInitialized = FALSE;
 
+// Cache for recent quotes
+struct QuoteCache {
+	CString symbol;
+	CString exchange;
+	float ltp;
+	float open;
+	float high;
+	float low;
+	float close;
+	float volume;
+	float oi;
+	DWORD lastUpdate;
+};
+
+static CMap<CString, LPCTSTR, QuoteCache, QuoteCache&> g_QuoteCache;
+
 typedef CArray< struct Quotation, struct Quotation > CQuoteArray;
 
 // Forward declarations
 VOID CALLBACK OnTimerProc(HWND, UINT, UINT_PTR, DWORD);
 void SetupRetry(void);
 BOOL TestOpenAlgoConnection(void);
+BOOL GetOpenAlgoQuote(LPCTSTR pszTicker, QuoteCache& quote);
+int GetOpenAlgoHistory(LPCTSTR pszTicker, int nPeriodicity, int nLastValid, int nSize, struct Quotation* pQuotes);
+CString GetExchangeFromTicker(LPCTSTR pszTicker);
+CString GetIntervalString(int nPeriodicity);
+void ConvertUnixToPackedDate(time_t unixTime, union AmiDate* pAmiDate);
 
 ///////////////////////////////
 // Helper Functions
@@ -69,6 +92,476 @@ CString BuildOpenAlgoURL(const CString& server, int port, const CString& endpoin
 	CString result;
 	result.Format(_T("http://%s:%d%s"), (LPCTSTR)server, port, (LPCTSTR)endpoint);
 	return result;
+}
+
+// Extract exchange from ticker format (e.g., "RELIANCE-NSE" -> "NSE")
+CString GetExchangeFromTicker(LPCTSTR pszTicker)
+{
+	CString ticker(pszTicker);
+	int dashPos = ticker.ReverseFind(_T('-'));
+	if (dashPos != -1)
+	{
+		return ticker.Mid(dashPos + 1);
+	}
+	// Default exchange if not specified
+	return _T("NSE");
+}
+
+// Get clean symbol without exchange suffix
+CString GetCleanSymbol(LPCTSTR pszTicker)
+{
+	CString ticker(pszTicker);
+	int dashPos = ticker.ReverseFind(_T('-'));
+	if (dashPos != -1)
+	{
+		return ticker.Left(dashPos);
+	}
+	return ticker;
+}
+
+// Convert periodicity to OpenAlgo interval string
+// Currently supporting only 1m and D (daily) intervals
+CString GetIntervalString(int nPeriodicity)
+{
+	// Only support 1-minute and Daily for now
+	if (nPeriodicity == 60)  // 1 minute in seconds
+		return _T("1m");
+	else
+		return _T("D");  // Default to daily for all other timeframes
+}
+
+// Convert Unix timestamp to AmiBroker date format
+void ConvertUnixToPackedDate(time_t unixTime, union AmiDate* pAmiDate)
+{
+	struct tm* timeinfo = localtime(&unixTime);
+
+	pAmiDate->PackDate.Year = timeinfo->tm_year + 1900;
+	pAmiDate->PackDate.Month = timeinfo->tm_mon + 1;
+	pAmiDate->PackDate.Day = timeinfo->tm_mday;
+	pAmiDate->PackDate.Hour = timeinfo->tm_hour;
+	pAmiDate->PackDate.Minute = timeinfo->tm_min;
+	pAmiDate->PackDate.Second = timeinfo->tm_sec;
+	pAmiDate->PackDate.MilliSec = 0;
+	pAmiDate->PackDate.MicroSec = 0;
+}
+
+// Fetch real-time quote from OpenAlgo
+BOOL GetOpenAlgoQuote(LPCTSTR pszTicker, QuoteCache& quote)
+{
+	AFX_MANAGE_STATE(AfxGetStaticModuleState());
+
+	if (g_oApiKey.IsEmpty())
+		return FALSE;
+
+	BOOL bSuccess = FALSE;
+
+	try
+	{
+		CString oURL = BuildOpenAlgoURL(g_oServer, g_nPortNumber, _T("/api/v1/quotes"));
+
+		// Prepare POST data
+		CString symbol = GetCleanSymbol(pszTicker);
+		CString exchange = GetExchangeFromTicker(pszTicker);
+
+		CString oPostData;
+		oPostData.Format(_T("{\"apikey\":\"%s\",\"symbol\":\"%s\",\"exchange\":\"%s\"}"),
+			(LPCTSTR)g_oApiKey, (LPCTSTR)symbol, (LPCTSTR)exchange);
+
+		CInternetSession oSession(AGENT_NAME, 1, INTERNET_OPEN_TYPE_DIRECT, NULL, NULL,
+			INTERNET_FLAG_DONT_CACHE);
+		oSession.SetOption(INTERNET_OPTION_CONNECT_TIMEOUT, 3000);
+		oSession.SetOption(INTERNET_OPTION_RECEIVE_TIMEOUT, 3000);
+
+		CHttpConnection* pConnection = NULL;
+		CHttpFile* pFile = NULL;
+
+		// Parse server
+		INTERNET_PORT nPort = (INTERNET_PORT)g_nPortNumber;
+		CString oServer = g_oServer;
+		oServer.Replace(_T("http://"), _T(""));
+		oServer.Replace(_T("https://"), _T(""));
+
+		pConnection = oSession.GetHttpConnection(oServer, nPort);
+
+		if (pConnection)
+		{
+			pFile = pConnection->OpenRequest(
+				CHttpConnection::HTTP_VERB_POST,
+				_T("/api/v1/quotes"),
+				NULL, 1, NULL, NULL,
+				INTERNET_FLAG_RELOAD | INTERNET_FLAG_DONT_CACHE);
+
+			if (pFile)
+			{
+				CString oHeaders = _T("Content-Type: application/json\r\n");
+				CStringA oPostDataA(oPostData);
+
+				if (pFile->SendRequest(oHeaders, (LPVOID)(LPCSTR)oPostDataA, oPostDataA.GetLength()))
+				{
+					DWORD dwStatusCode = 0;
+					pFile->QueryInfoStatusCode(dwStatusCode);
+
+					if (dwStatusCode == 200)
+					{
+						CString oResponse;
+						CString oLine;
+						while (pFile->ReadString(oLine))
+						{
+							oResponse += oLine;
+						}
+
+						// Parse JSON response (simple parsing)
+						if (oResponse.Find(_T("\"status\":\"success\"")) >= 0)
+						{
+							// Extract values using simple string parsing
+							int pos;
+
+							// Parse LTP
+							pos = oResponse.Find(_T("\"ltp\":"));
+							if (pos >= 0)
+							{
+								pos += 6;
+								int endPos = oResponse.Find(_T(","), pos);
+								if (endPos < 0) endPos = oResponse.Find(_T("}"), pos);
+								CString val = oResponse.Mid(pos, endPos - pos);
+								quote.ltp = (float)_tstof(val);
+							}
+
+							// Parse Open
+							pos = oResponse.Find(_T("\"open\":"));
+							if (pos >= 0)
+							{
+								pos += 7;
+								int endPos = oResponse.Find(_T(","), pos);
+								if (endPos < 0) endPos = oResponse.Find(_T("}"), pos);
+								CString val = oResponse.Mid(pos, endPos - pos);
+								quote.open = (float)_tstof(val);
+							}
+
+							// Parse High
+							pos = oResponse.Find(_T("\"high\":"));
+							if (pos >= 0)
+							{
+								pos += 7;
+								int endPos = oResponse.Find(_T(","), pos);
+								if (endPos < 0) endPos = oResponse.Find(_T("}"), pos);
+								CString val = oResponse.Mid(pos, endPos - pos);
+								quote.high = (float)_tstof(val);
+							}
+
+							// Parse Low
+							pos = oResponse.Find(_T("\"low\":"));
+							if (pos >= 0)
+							{
+								pos += 6;
+								int endPos = oResponse.Find(_T(","), pos);
+								if (endPos < 0) endPos = oResponse.Find(_T("}"), pos);
+								CString val = oResponse.Mid(pos, endPos - pos);
+								quote.low = (float)_tstof(val);
+							}
+
+							// Parse Volume
+							pos = oResponse.Find(_T("\"volume\":"));
+							if (pos >= 0)
+							{
+								pos += 9;
+								int endPos = oResponse.Find(_T(","), pos);
+								if (endPos < 0) endPos = oResponse.Find(_T("}"), pos);
+								CString val = oResponse.Mid(pos, endPos - pos);
+								quote.volume = (float)_tstof(val);
+							}
+
+							// Parse OI
+							pos = oResponse.Find(_T("\"oi\":"));
+							if (pos >= 0)
+							{
+								pos += 5;
+								int endPos = oResponse.Find(_T(","), pos);
+								if (endPos < 0) endPos = oResponse.Find(_T("}"), pos);
+								CString val = oResponse.Mid(pos, endPos - pos);
+								quote.oi = (float)_tstof(val);
+							}
+
+							// Parse Previous Close
+							pos = oResponse.Find(_T("\"prev_close\":"));
+							if (pos >= 0)
+							{
+								pos += 13;
+								int endPos = oResponse.Find(_T(","), pos);
+								if (endPos < 0) endPos = oResponse.Find(_T("}"), pos);
+								CString val = oResponse.Mid(pos, endPos - pos);
+								quote.close = (float)_tstof(val);
+							}
+
+							quote.symbol = symbol;
+							quote.exchange = exchange;
+							quote.lastUpdate = GetTickCount();
+
+							bSuccess = TRUE;
+						}
+					}
+				}
+
+				pFile->Close();
+				delete pFile;
+			}
+
+			pConnection->Close();
+			delete pConnection;
+		}
+
+		oSession.Close();
+	}
+	catch (CInternetException* e)
+	{
+		e->Delete();
+	}
+
+	return bSuccess;
+}
+
+// Fetch historical data from OpenAlgo
+// Currently supports only 1m and D (daily) intervals
+int GetOpenAlgoHistory(LPCTSTR pszTicker, int nPeriodicity, int nLastValid, int nSize, struct Quotation* pQuotes)
+{
+	AFX_MANAGE_STATE(AfxGetStaticModuleState());
+
+	if (g_oApiKey.IsEmpty())
+		return nLastValid + 1;
+
+	try
+	{
+		CString oURL = BuildOpenAlgoURL(g_oServer, g_nPortNumber, _T("/api/v1/history"));
+
+		// Prepare POST data
+		CString symbol = GetCleanSymbol(pszTicker);
+		CString exchange = GetExchangeFromTicker(pszTicker);
+		CString interval = GetIntervalString(nPeriodicity);
+
+		// Calculate date range based on interval type
+		CTime endTime = CTime::GetCurrentTime();
+		CTime startTime = endTime;
+
+		// Determine how far back to request
+		if (nPeriodicity == 60) // 1-minute data
+		{
+			// For 1-minute data, get last 5 days
+			startTime -= CTimeSpan(5, 0, 0, 0);
+		}
+		else // Daily data
+		{
+			// For daily data, get last 1 year
+			startTime -= CTimeSpan(365, 0, 0, 0);
+		}
+
+		CString startDate = startTime.Format(_T("%Y-%m-%d"));
+		CString endDate = endTime.Format(_T("%Y-%m-%d"));
+
+		CString oPostData;
+		oPostData.Format(_T("{\"apikey\":\"%s\",\"symbol\":\"%s\",\"exchange\":\"%s\",\"interval\":\"%s\",\"start_date\":\"%s\",\"end_date\":\"%s\"}"),
+			(LPCTSTR)g_oApiKey, (LPCTSTR)symbol, (LPCTSTR)exchange, (LPCTSTR)interval, (LPCTSTR)startDate, (LPCTSTR)endDate);
+
+		CInternetSession oSession(AGENT_NAME, 1, INTERNET_OPEN_TYPE_DIRECT, NULL, NULL,
+			INTERNET_FLAG_DONT_CACHE);
+		oSession.SetOption(INTERNET_OPTION_CONNECT_TIMEOUT, 10000);
+		oSession.SetOption(INTERNET_OPTION_RECEIVE_TIMEOUT, 10000);
+
+		CHttpConnection* pConnection = NULL;
+		CHttpFile* pFile = NULL;
+
+		// Parse server
+		INTERNET_PORT nPort = (INTERNET_PORT)g_nPortNumber;
+		CString oServer = g_oServer;
+		oServer.Replace(_T("http://"), _T(""));
+		oServer.Replace(_T("https://"), _T(""));
+
+		pConnection = oSession.GetHttpConnection(oServer, nPort);
+
+		if (pConnection)
+		{
+			pFile = pConnection->OpenRequest(
+				CHttpConnection::HTTP_VERB_POST,
+				_T("/api/v1/history"),
+				NULL, 1, NULL, NULL,
+				INTERNET_FLAG_RELOAD | INTERNET_FLAG_DONT_CACHE);
+
+			if (pFile)
+			{
+				CString oHeaders = _T("Content-Type: application/json\r\n");
+				CStringA oPostDataA(oPostData);
+
+				if (pFile->SendRequest(oHeaders, (LPVOID)(LPCSTR)oPostDataA, oPostDataA.GetLength()))
+				{
+					DWORD dwStatusCode = 0;
+					pFile->QueryInfoStatusCode(dwStatusCode);
+
+					if (dwStatusCode == 200)
+					{
+						CString oResponse;
+						CString oLine;
+						while (pFile->ReadString(oLine))
+						{
+							oResponse += oLine;
+						}
+
+						// Parse JSON response
+						if (oResponse.Find(_T("\"status\":\"success\"")) >= 0)
+						{
+							// Find data array
+							int dataStart = oResponse.Find(_T("\"data\":["));
+							if (dataStart >= 0)
+							{
+								dataStart += 8;
+								int dataEnd = oResponse.Find(_T("]"), dataStart);
+								CString dataArray = oResponse.Mid(dataStart, dataEnd - dataStart);
+
+								// Parse each candle
+								int quoteIndex = nLastValid + 1;
+								int pos = 0;
+
+								while (pos < dataArray.GetLength() && quoteIndex < nSize)
+								{
+									int candleStart = dataArray.Find(_T("{"), pos);
+									if (candleStart < 0) break;
+
+									int candleEnd = dataArray.Find(_T("}"), candleStart);
+									if (candleEnd < 0) break;
+
+									CString candle = dataArray.Mid(candleStart, candleEnd - candleStart + 1);
+
+									// Parse timestamp
+									int tsPos = candle.Find(_T("\"timestamp\":"));
+									if (tsPos >= 0)
+									{
+										tsPos += 12;
+										int tsEnd = candle.Find(_T(","), tsPos);
+										if (tsEnd < 0) tsEnd = candle.Find(_T("}"), tsPos);
+										CString tsStr = candle.Mid(tsPos, tsEnd - tsPos);
+										time_t timestamp = (time_t)_tstoi64(tsStr);
+
+										// Convert to AmiBroker date
+										ConvertUnixToPackedDate(timestamp, &pQuotes[quoteIndex].DateTime);
+
+										// Parse OHLCV
+										int oPos = candle.Find(_T("\"open\":"));
+										if (oPos >= 0)
+										{
+											oPos += 7;
+											int oEnd = candle.Find(_T(","), oPos);
+											CString val = candle.Mid(oPos, oEnd - oPos);
+											pQuotes[quoteIndex].Open = (float)_tstof(val);
+										}
+
+										int hPos = candle.Find(_T("\"high\":"));
+										if (hPos >= 0)
+										{
+											hPos += 7;
+											int hEnd = candle.Find(_T(","), hPos);
+											CString val = candle.Mid(hPos, hEnd - hPos);
+											pQuotes[quoteIndex].High = (float)_tstof(val);
+										}
+
+										int lPos = candle.Find(_T("\"low\":"));
+										if (lPos >= 0)
+										{
+											lPos += 6;
+											int lEnd = candle.Find(_T(","), lPos);
+											CString val = candle.Mid(lPos, lEnd - lPos);
+											pQuotes[quoteIndex].Low = (float)_tstof(val);
+										}
+
+										int cPos = candle.Find(_T("\"close\":"));
+										if (cPos >= 0)
+										{
+											cPos += 8;
+											int cEnd = candle.Find(_T(","), cPos);
+											if (cEnd < 0) cEnd = candle.Find(_T("}"), cPos);
+											CString val = candle.Mid(cPos, cEnd - cPos);
+											pQuotes[quoteIndex].Price = (float)_tstof(val);
+										}
+
+										int vPos = candle.Find(_T("\"volume\":"));
+										if (vPos >= 0)
+										{
+											vPos += 9;
+											int vEnd = candle.Find(_T(","), vPos);
+											if (vEnd < 0) vEnd = candle.Find(_T("}"), vPos);
+											CString val = candle.Mid(vPos, vEnd - vPos);
+											pQuotes[quoteIndex].Volume = (float)_tstof(val);
+										}
+
+										int oiPos = candle.Find(_T("\"oi\":"));
+										if (oiPos >= 0)
+										{
+											oiPos += 5;
+											int oiEnd = candle.Find(_T(","), oiPos);
+											if (oiEnd < 0) oiEnd = candle.Find(_T("}"), oiPos);
+											CString val = candle.Mid(oiPos, oiEnd - oiPos);
+											pQuotes[quoteIndex].OpenInterest = (float)_tstof(val);
+										}
+
+										// Set auxiliary data
+										pQuotes[quoteIndex].AuxData1 = 0;
+										pQuotes[quoteIndex].AuxData2 = 0;
+
+										quoteIndex++;
+									}
+
+									pos = candleEnd + 1;
+								}
+
+								// Update with latest quote if available for intraday data
+								if (quoteIndex > nLastValid + 1 && nPeriodicity == 60)
+								{
+									QuoteCache latestQuote;
+									if (GetOpenAlgoQuote(pszTicker, latestQuote))
+									{
+										// Update the last bar with real-time data
+										if (quoteIndex > 0)
+										{
+											pQuotes[quoteIndex - 1].Price = latestQuote.ltp;
+											pQuotes[quoteIndex - 1].High = max(pQuotes[quoteIndex - 1].High, latestQuote.ltp);
+											pQuotes[quoteIndex - 1].Low = min(pQuotes[quoteIndex - 1].Low, latestQuote.ltp);
+											pQuotes[quoteIndex - 1].Volume = latestQuote.volume;
+											pQuotes[quoteIndex - 1].OpenInterest = latestQuote.oi;
+										}
+									}
+								}
+
+								pFile->Close();
+								delete pFile;
+								pConnection->Close();
+								delete pConnection;
+								oSession.Close();
+
+								return quoteIndex;
+							}
+						}
+					}
+				}
+
+				if (pFile)
+				{
+					pFile->Close();
+					delete pFile;
+				}
+			}
+
+			if (pConnection)
+			{
+				pConnection->Close();
+				delete pConnection;
+			}
+		}
+
+		oSession.Close();
+	}
+	catch (CInternetException* e)
+	{
+		e->Delete();
+	}
+
+	return nLastValid + 1;
 }
 
 BOOL AddToOpenAlgoPortfolio(LPCTSTR pszTicker)
@@ -84,8 +577,6 @@ BOOL AddToOpenAlgoPortfolio(LPCTSTR pszTicker)
 
 		CInternetSession oSession(AGENT_NAME, 1, INTERNET_OPEN_TYPE_DIRECT, NULL, NULL, INTERNET_FLAG_DONT_CACHE);
 
-		// For watchlist endpoint, we might need to add API key as header or parameter
-		// This depends on OpenAlgo's implementation
 		CStdioFile* poFile = oSession.OpenURL(oURL, 1, INTERNET_FLAG_TRANSFER_ASCII | INTERNET_FLAG_RELOAD | INTERNET_FLAG_DONT_CACHE);
 
 		CString oLine;
@@ -143,6 +634,9 @@ PLUGINAPI int Init(void)
 
 		g_nStatus = STATUS_WAIT;
 		g_bPluginInitialized = TRUE;
+
+		// Initialize quote cache
+		g_QuoteCache.InitHashTable(997); // Prime number for better hash distribution
 	}
 
 	return 1;
@@ -151,6 +645,10 @@ PLUGINAPI int Init(void)
 PLUGINAPI int Release(void)
 {
 	AFX_MANAGE_STATE(AfxGetStaticModuleState());
+
+	// Clear cache
+	g_QuoteCache.RemoveAll();
+
 	return 1;
 }
 
@@ -492,6 +990,9 @@ PLUGINAPI int Notify(struct PluginNotification* pn)
 		free(g_aInfos);
 		g_aInfos = NULL;
 		RecentInfoSize = 0;
+
+		// Clear cache
+		g_QuoteCache.RemoveAll();
 	}
 
 	// Right-click on status area - show menu
@@ -584,12 +1085,80 @@ PLUGINAPI int GetQuotesEx(LPCTSTR pszTicker, int nPeriodicity, int nLastValid, i
 		return nLastValid + 1;
 	}
 
-	// TODO: Implement actual quote fetching using OpenAlgo API with authentication
-	// This will need to use the g_oApiKey for authentication
-	return nLastValid + 1;
+	// Only support 1-minute and Daily intervals
+	// For other intervals, just return existing data
+	if (nPeriodicity != 60 && nPeriodicity != 86400)
+	{
+		// Unsupported interval - return existing data
+		return nLastValid + 1;
+	}
+
+	// Fetch historical data from OpenAlgo
+	int nQty = GetOpenAlgoHistory(pszTicker, nPeriodicity, nLastValid, nSize, pQuotes);
+
+	return nQty;
 }
 
 PLUGINAPI struct RecentInfo* GetRecentInfo(LPCTSTR pszTicker)
 {
-	return NULL;
+	AFX_MANAGE_STATE(AfxGetStaticModuleState());
+
+	// Check if we're connected and have an API key
+	if (g_nStatus != STATUS_CONNECTED || g_oApiKey.IsEmpty())
+		return NULL;
+
+	static struct RecentInfo ri;
+	memset(&ri, 0, sizeof(ri));
+	ri.nStructSize = sizeof(struct RecentInfo);
+
+	// Check cache first
+	QuoteCache cachedQuote;
+	CString ticker(pszTicker);
+	BOOL bCached = FALSE;
+
+	if (g_QuoteCache.Lookup(ticker, cachedQuote))
+	{
+		// Use cached data if it's less than 5 seconds old
+		DWORD dwNow = GetTickCount();
+		if ((dwNow - cachedQuote.lastUpdate) < 5000)
+		{
+			bCached = TRUE;
+		}
+	}
+
+	// Fetch new quote if not cached or cache is stale
+	if (!bCached)
+	{
+		if (!GetOpenAlgoQuote(pszTicker, cachedQuote))
+			return NULL;
+
+		// Store in cache
+		g_QuoteCache.SetAt(ticker, cachedQuote);
+	}
+
+	// Fill RecentInfo structure
+	_tcsncpy_s(ri.Name, sizeof(ri.Name) / sizeof(TCHAR), pszTicker, _TRUNCATE);
+	_tcsncpy_s(ri.Exchange, sizeof(ri.Exchange) / sizeof(TCHAR), cachedQuote.exchange, _TRUNCATE);
+
+	ri.nStatus = RI_STATUS_UPDATE | RI_STATUS_TRADE | RI_STATUS_BARSREADY;
+	ri.nBitmap = RI_LAST | RI_OPEN | RI_HIGHLOW | RI_TRADEVOL | RI_OPENINT;
+
+	ri.fLast = cachedQuote.ltp;
+	ri.fOpen = cachedQuote.open;
+	ri.fHigh = cachedQuote.high;
+	ri.fLow = cachedQuote.low;
+	ri.fPrev = cachedQuote.close;
+	ri.fChange = cachedQuote.ltp - cachedQuote.close;
+	ri.fTradeVol = cachedQuote.volume;
+	ri.fTotalVol = cachedQuote.volume;
+	ri.fOpenInt = cachedQuote.oi;
+
+	// Set update times
+	CTime now = CTime::GetCurrentTime();
+	ri.nDateUpdate = now.GetYear() * 10000 + now.GetMonth() * 100 + now.GetDay();
+	ri.nTimeUpdate = now.GetHour() * 10000 + now.GetMinute() * 100 + now.GetSecond();
+	ri.nDateChange = ri.nDateUpdate;
+	ri.nTimeChange = ri.nTimeUpdate;
+
+	return &ri;
 }
